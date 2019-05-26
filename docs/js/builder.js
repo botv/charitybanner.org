1494c7b9096642f46e0218a8907b611722246046
Ben Botvinick <benbotvinick@gmail.com>
2019-05-26 10:49:43 -0400

    fixed builder

--- docs/js/builder.js
+++ docs/js/builder.js
@@ -1,20 +1,8 @@
+import uniqid from 'uniqid'
+
 export default function builder(callback) {
 	$.get('https://us-central1-charity-banner.cloudfunctions.net/getAllTagIds', (tagIds) => {
-		console.log(tagIds);
-
-		function makeId(length) {
-			let result = '';
-			let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-			let charactersLength = characters.length;
-			for (let i = 0; i < length; i++) {
-				result += characters.charAt(Math.floor(Math.random() * charactersLength));
-			}
-
-			return result;
-		}
-
-		let id = makeId(30);
-		while (tagIds.includes(id)) id = makeId(30);
+		const bannerId = uniqid();
 
 		$.get('https://us-central1-charity-banner.cloudfunctions.net/initAnalytics',
 			{bannerId: bannerId}
@@ -23,7 +11,7 @@ export default function builder(callback) {
 		const url = new URL('https://us-central1-charity-banner.cloudfunctions.net/banner');
 		url.searchParams.set('projectId', this.projectId);
 		url.searchParams.set('style', this.style);
-		url.searchParams.set('bannerId', id);
+		url.searchParams.set('bannerId', bannerId);
 
 		callback(`<script src="${url.href}" defer></script>`, id);
 	});
